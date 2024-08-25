454a4eae2ef0f3c1512207224ba3a33b011d4989
Parthib Dhar <91363085+parthibdhar@users.noreply.github.com>
2024-08-25 19:15:57 +0530

    done more

---
+++ .history/Patterns/25thAug24/Patterns/B1824_20240825182403.java
@@ -0,0 +1,9 @@
+
+public class B1824 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number: ");
+            int n = sc.nextInt();
+            for (int i = 1; i <= n; i++) {
+}
---
+++ .history/Patterns/25thAug24/Patterns/B1824_20240825182801.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class B1824 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number: ");
+            int n = sc.nextInt();
+            for (int i = 1; i <= 2*n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(" *");
+                }
+                for (int j = 1; j < 2*n-i; j++) {
+                    System.out.print("  ");
+                }
+                for (int j = 1; j <= i; j++) {
+                    System.out.print(" *");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
