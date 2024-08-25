6cc6d3687794902d39ad964b725a36b9ab41d0b0
Parthib Dhar <91363085+parthibdhar@users.noreply.github.com>
2024-08-25 18:11:23 +0530

    added

---
+++ .history/Patterns/25thAug24/Patterns/B1802_20240825180405.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class B1802 {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the number: ");
+            int n = sc.nextInt();
+            int k = 1;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <=  i ; j++) {
+                    System.out.print( (k % 2) + " ");
+                    k++;
+                }
+                System.out.println();
+            }
+        }
+    }
+}
